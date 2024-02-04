3017c65ab70a82795ad5cb1a2f75a1c42a920d3d
IamNayanIslam <iamnayanislam@gmail.com>
2024-02-04 15:26:22 +0600

    features section complete

--- src/App.jsx
+++ src/App.jsx
@@ -44,21 +44,21 @@ const App = () => {
 
       {/* ----------------------------------------Features----------------------------------------*/}
       <div className="bg-indigo-800 text-indigo-200">
-      <div className="max-w-screen-lg mx-auto flex py-16 px-10">
-        <div>
-          <img src="https://images.unsplash.com/photo-1586179253019-ac8a2522535f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80" alt="" />
+      <div className="max-w-screen-lg mx-auto md:flex lg:flex space-x-2 py-16 px-10 text-center">
+        <div className="mb-4 md:mb-0 lg:mb-0">
+          <img src="https://images.unsplash.com/photo-1586179253019-ac8a2522535f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80" alt="" className="w-full mr-2 mb-4 rounded border-solid border-2 border-indigo-400"/>
           <p>Cuddles!</p>
         </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1453369569379-52cd4fc989af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80" alt="" />
+        <div className="mb-4 md:mb-0 lg:mb-0">
+          <img src="https://images.unsplash.com/photo-1453369569379-52cd4fc989af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80" alt="" className="w-full mr-2 mb-4 rounded border-solid border-2 border-indigo-400 md:mt-8 lg:mt-8"/>
           <p>Smiles!</p>
         </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80" alt="" />
+        <div className="mb-4 md:mb-0 lg:mb-0">
+          <img src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80" alt="" className="w-full mr-2 mb-4 rounded border-solid border-2 border-indigo-400"/>
           <p>Games!</p>
         </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1505623776320-7edecf5f0771?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="" />
+        <div className="mb-4 md:mb-0 lg:mb-0">
+          <img src="https://images.unsplash.com/photo-1505623776320-7edecf5f0771?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="" className="w-full mr-2 mb-4 rounded border-solid border-2 border-indigo-400 md:mt-8 lg:mt-8"/>
           <p>Swims!</p>
         </div>
       </div>
